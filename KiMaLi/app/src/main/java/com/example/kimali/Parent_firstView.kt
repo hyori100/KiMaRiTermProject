18291aa42e9aae596d307d4c174b2fb84aa846db
MaHyori <ag497752@naver.com>
2019-11-28 22:50:16 +0900

    Listview_design hyori commmit

--- KiMaLi/app/src/main/java/com/example/kimali/Parent_firstView.kt
+++ KiMaLi/app/src/main/java/com/example/kimali/Parent_firstView.kt
@@ -1,9 +1,11 @@
 package com.example.kimali
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.ListView
+import android.widget.Toast
 
 class Parent_firstView : AppCompatActivity() {
     val array: Array<String> = arrayOf("아들1","아들2","아들3","딸1","딸2")
@@ -16,6 +18,13 @@ class Parent_firstView : AppCompatActivity() {
         val adapter: ArrayAdapter<String> = ArrayAdapter(this, android.R.layout.simple_list_item_1,array)
 
         listview.adapter = adapter
+        listview.setOnItemClickListener { parent, view, position, id ->
+            val selectedItem = parent.getItemAtPosition(position) as String
+            Toast.makeText(this, "Clicked item :"+" "+selectedItem, Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, Parent_missionList::class.java)
+            intent.putExtra("selectedString", selectedItem)
+            this.startActivity(intent)
+        }
 
 
     }
