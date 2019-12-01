015214fbb32090b07750d64594758bbc5ec00810
Limchanho <chwow0710@gmail.com>
2019-12-01 18:38:00 +0900

    pc 사용시간 확인 화면 전부 완료

--- KiMaLi/app/src/main/java/com/example/kimali/compensation/compensation_pc.kt
+++ KiMaLi/app/src/main/java/com/example/kimali/compensation/compensation_pc.kt
@@ -1,7 +1,11 @@
 package com.example.kimali.compensation
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import com.example.kimali.R
 import com.example.kimali.text
@@ -17,6 +21,26 @@ class compensation_pc : AppCompatActivity() {
             setTitle(text)
         } else {
             Toast.makeText(this, "전달된 이름이 없습니다", Toast.LENGTH_SHORT).show()
+        }
+
+        var pc : Int = 9  // 이곳에 데이터베이스에 다 더해진 pc 사용 시간을 가지고 와야함
+
+        var pc_text = findViewById(R.id.pc_text) as TextView
+        var use_pc = findViewById(R.id.use_pc) as EditText
+        var use_pc_btn = findViewById(R.id.use_pc_btn) as Button
+        var confirm_pc_btn = findViewById(R.id.confirm_pc_btn) as Button
+
+        pc_text.setText("pc 사용 가능 시간 : " + pc + " 시간")
+
+        use_pc_btn.setOnClickListener {
+            var i : String = use_pc.text.toString();
+            Toast.makeText(this, i, Toast.LENGTH_SHORT).show()
+        }
+
+        confirm_pc_btn.setOnClickListener {
+            val intent = Intent(this, compensation_firstActivity::class.java)
+            intent.putExtra("selectedString", text)
+            this.startActivity(intent)
         }
     }
 }
