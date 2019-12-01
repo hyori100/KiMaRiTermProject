3479cbb6b807e748f5e82e28a34fbc0c785e841e
Limchanho <chwow0710@gmail.com>
2019-12-01 18:54:05 +0900

    자식부모인텐트오류해결

--- KiMaLi/app/src/main/java/com/example/kimali/BridgeActivity.kt
+++ KiMaLi/app/src/main/java/com/example/kimali/BridgeActivity.kt
@@ -16,12 +16,17 @@ class BridgeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_bridge)
 
-        if (intent.hasExtra("selectedString")) {
+        if (intent.hasExtra("id")) {
+            text = intent.getStringExtra("id")
+            setTitle(text)
+        } else if(intent.hasExtra("selectedString")){
             text = intent.getStringExtra("selectedString")
             setTitle(text)
         } else {
             Toast.makeText(this, "전달된 이름이 없습니다", Toast.LENGTH_SHORT).show()
         }
+
+
 
         var mission_list_btn = findViewById(R.id.mission_list_btn) as Button
         var reward_btn = findViewById(R.id.reward_btn) as Button
