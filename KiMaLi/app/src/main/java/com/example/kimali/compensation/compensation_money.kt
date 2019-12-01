5ff721aa24878c388154ebd0a0dfa5e150b39a38
Limchanho <chwow0710@gmail.com>
2019-12-01 21:41:11 +0900

    make alert dialog and fix code

--- KiMaLi/app/src/main/java/com/example/kimali/compensation/compensation_money.kt
+++ KiMaLi/app/src/main/java/com/example/kimali/compensation/compensation_money.kt
@@ -1,14 +1,25 @@
 package com.example.kimali.compensation
 
+import android.content.DialogInterface
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.util.Log
+import android.view.MotionEvent
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import com.example.kimali.Login.Loginactivity
 import com.example.kimali.R
 import com.example.kimali.text
+import kotlinx.android.synthetic.main.activity_compensation_money.*
+import kotlinx.android.synthetic.main.activity_parent_listview.*
+
+var money_m : Int = 0
 
 class compensation_money : AppCompatActivity() {
 
@@ -23,19 +34,25 @@ class compensation_money : AppCompatActivity() {
             Toast.makeText(this, "전달된 이름이 없습니다", Toast.LENGTH_SHORT).show()
         }
 
-
-        var money : Int = 1000  // 이곳에 데이터베이스에 다 더해진 돈을 가지고 와야함
+        money_m = 100000 // 이곳에 데이터베이스에 다 더해진 돈을 가지고 와야함
 
         var money_text = findViewById(R.id.money_text) as TextView
         var use_money = findViewById(R.id.use_money) as EditText
         var use_money_btn = findViewById(R.id.use_money_btn) as Button
         var confirm_money_btn = findViewById(R.id.confirm_money_btn) as Button
 
-        money_text.setText("총 보상 금액 : " + money + " 원")
+        money_text.setText("총 보상 금액 : " + money_m + " 원")
+
+
 
         use_money_btn.setOnClickListener {
-            var i : String = use_money.text.toString();
-            Toast.makeText(this, i, Toast.LENGTH_SHORT).show()
+            var i : Int
+            if(!TextUtils.isEmpty(use_money.text.toString())) {
+                i = Integer.parseInt(use_money.text.toString())
+            }
+            else
+                i = 0
+            use_money(i)
         }
 
         confirm_money_btn.setOnClickListener {
@@ -44,4 +61,50 @@ class compensation_money : AppCompatActivity() {
             this.startActivity(intent)
         }
     }
+    // editText에 입력 후 버튼 누르면 결제 될지 안될지 정해주는 창
+    fun use_money(i : Int) {
+        //Toast.makeText(this,i,Toast.LENGTH_SHORT).show()
+        Log.d("chano",i.toString())
+        if (i == 0) {
+            val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+            //tilte 부분 xml
+            builder.setTitle("알림")
+            builder.setMessage("금액을 입력해주세요")
+            //확인버튼
+            builder.setPositiveButton("확인",
+                DialogInterface.OnClickListener { dialog, which ->
+                })
+            builder.show()
+        } else if (money_m >= i) {
+            val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+            //tilte 부분 xml
+            builder.setTitle("알림")
+            builder.setMessage("총 보상금액에서 " + i + " 원 사용하시겠습니까?")
+            //확인버튼
+            builder.setPositiveButton("확인",
+                DialogInterface.OnClickListener { dialog, which ->
+                    money_m = money_m - i  // 여기서 다시 파이어베이스에 저장해줘야함
+                    money_text.setText("총 보상 금액 : " + money_m + " 원")
+                    use_money.setText("")
+                })
+
+            builder.setNegativeButton("취소",
+                DialogInterface.OnClickListener { dialog, which -> })
+            builder.show()
+        } else {
+            val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+            //tilte 부분 xml
+            builder.setTitle("알림")
+            builder.setMessage("입력한 금액이 총 금액보다 많습니다.")
+            //확인버튼
+            builder.setPositiveButton("확인",
+                DialogInterface.OnClickListener { dialog, which ->
+                    use_money.setText("")
+                })
+            builder.show()
+        }
+    }
+
+
+
 }
--- KiMaLi/app/src/main/java/com/example/kimali/compensation/compensation_pc.kt
+++ KiMaLi/app/src/main/java/com/example/kimali/compensation/compensation_pc.kt
@@ -1,14 +1,21 @@
 package com.example.kimali.compensation
 
+import android.content.DialogInterface
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import com.example.kimali.R
 import com.example.kimali.text
+import kotlinx.android.synthetic.main.activity_compensation_pc.*
+
+var pc : Int = 0
 
 class compensation_pc : AppCompatActivity() {
 
@@ -23,7 +30,7 @@ class compensation_pc : AppCompatActivity() {
             Toast.makeText(this, "전달된 이름이 없습니다", Toast.LENGTH_SHORT).show()
         }
 
-        var pc : Int = 9  // 이곳에 데이터베이스에 다 더해진 pc 사용 시간을 가지고 와야함
+         pc = 9  // 이곳에 데이터베이스에 다 더해진 pc 사용 시간을 가지고 와야함
 
         var pc_text = findViewById(R.id.pc_text) as TextView
         var use_pc = findViewById(R.id.use_pc) as EditText
@@ -32,15 +39,62 @@ class compensation_pc : AppCompatActivity() {
 
         pc_text.setText("pc 사용 가능 시간 : " + pc + " 시간")
 
+
         use_pc_btn.setOnClickListener {
-            var i : String = use_pc.text.toString();
-            Toast.makeText(this, i, Toast.LENGTH_SHORT).show()
+            var i : Int
+            if(!TextUtils.isEmpty(use_pc.text.toString())) {
+                i = Integer.parseInt(use_pc.text.toString())
+            }
+            else
+                i = 0
+            use_pc(i)
         }
 
         confirm_pc_btn.setOnClickListener {
             val intent = Intent(this, compensation_firstActivity::class.java)
             intent.putExtra("selectedString", text)
             this.startActivity(intent)
+        }
+    }
+
+    fun use_pc(i : Int) {
+        if (i == 0) {
+            val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+            //tilte 부분 xml
+            builder.setTitle("알림")
+            builder.setMessage("시간을 입력해주세요")
+            //확인버튼
+            builder.setPositiveButton("확인",
+                DialogInterface.OnClickListener { dialog, which ->
+                })
+            builder.show()
+        } else if (pc >= i) {
+            val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+            //tilte 부분 xml
+            builder.setTitle("알림")
+            builder.setMessage("총 사용시간에서 " + i + " 시간 사용하시겠습니까?")
+            //확인버튼
+            builder.setPositiveButton("확인",
+                DialogInterface.OnClickListener { dialog, which ->
+                    pc = pc - i  // 여기서 다시 파이어베이스에 저장해줘야함
+                    pc_text.setText("pc 사용 가능 시간 : " + pc + " 시간")
+                    use_pc.setText("")
+                })
+
+            builder.setNegativeButton("취소",
+                DialogInterface.OnClickListener { dialog, which -> })
+            builder.show()
+        } else {
+            val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+            //tilte 부분 xml
+            builder.setTitle("알림")
+            builder.setMessage("입력한 시간이 총 시간보다 많습니다.")
+            //확인버튼
+            builder.setPositiveButton("확인",
+                DialogInterface.OnClickListener { dialog, which ->
+                    use_pc.setText("")
+                })
+            builder.show()
         }
     }
 }
