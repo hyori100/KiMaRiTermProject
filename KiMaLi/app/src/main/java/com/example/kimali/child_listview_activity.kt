a8deb4272bd215d301f7a699e134b4caca5d1261
Limchanho <chwow0710@gmail.com>
2019-12-04 13:41:35 +0900

    listview 자녀 부모 나눠서 다시 만듬& 보상화면 오류 수정

--- KiMaLi/app/src/main/java/com/example/kimali/child_listview_activity.kt
+++ KiMaLi/app/src/main/java/com/example/kimali/child_listview_activity.kt
@@ -9,7 +9,7 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_compensation_money.*
 
-class parent_listview_activity : AppCompatActivity() {
+class child_listview_activity : AppCompatActivity() {
 
     lateinit var text : String
     lateinit var who : String
@@ -23,16 +23,12 @@ class parent_listview_activity : AppCompatActivity() {
         } else {
             Toast.makeText(this, "전달된 이름이 없습니다", Toast.LENGTH_SHORT).show()
         }
-
-
         super.onCreate(savedInstanceState)
         setTheme(R.style.AppTheme_NoActionBar)
-        setContentView(R.layout.activity_parent_listview)
+        setContentView(R.layout.activity_child_listview_activity)
 
 
         val okButton=findViewById<Button>(R.id.ok_mission_button)
-        val modifyButton=findViewById<Button>(R.id.modify_mission_button)
-        val deleteButton=findViewById<Button>(R.id.delete_mission_button)
 
         val deadLineText=findViewById<TextView>(R.id.deadline_textview)
         val moneyText=findViewById<TextView>(R.id.moneyText)
@@ -42,29 +38,13 @@ class parent_listview_activity : AppCompatActivity() {
         val missionName=findViewById<TextView>(R.id.mission_Name_TextView)
 
         okButton.setOnClickListener { view->
+            // 여기서 버튼을 누르면 부모에게 알림이 전송될 수 있게 설정해줘야함
             val intent = Intent(this, Parent_missionList::class.java)
             intent.putExtra("selectedString", text)
             intent.putExtra("who",who)
             //여기에 파이어베이스에서 현재 자녀의 이름 가지고오는 코드가 들어가야함..//
             this.startActivity(intent)
         }
-        // 이부분이 수정버튼 클릭시
-        modifyButton.setOnClickListener { view->
-            // 여기서 파이어베이스에 현재 바뀐 내용을 저장할 수 있는 코드 작성해야함
-            val intent = Intent(this, Parent_missionList::class.java)
-            intent.putExtra("selectedString", text)
-            intent.putExtra("who",who)
-            this.startActivity(intent)
-        }
-        // 삭제하기 버튼
-        deleteButton.setOnClickListener { view->
-            // 삭제 시 파이어베이스에서 현재 내용을 전부 다 delete
-            val intent = Intent(this, Parent_missionList::class.java)
-            intent.putExtra("selectedString", text)
-            intent.putExtra("who",who)
-            this.startActivity(intent)
-        }
-
     }
 
 }
